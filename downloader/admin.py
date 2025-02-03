fdcee9684639a322ddfad2e3826e47a519962dee
Rauf Masoumi <raouf.rm93@gmail.com>
2025-02-03 12:23:55 +0330

    Added Content model for storing the processed files data

--- downloader/admin.py
+++ downloader/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Content
 # Register your models here.
+
+
+class ContentAdmin(admin.ModelAdmin):
+    model = Content
+    list_display = ['pk', 'processed_at', 'expiration_date']
+
+
+admin.site.register(Content, ContentAdmin)
--- downloader/models.py
+++ downloader/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from datetime import timedelta
+import uuid
 # Create your models here.
+
+
+class Content(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=300, blank=True, null=True)
+    download_url = models.URLField(blank=True, null=True)
+    download_path = models.FilePathField(blank=True, null=True)
+    processed_at = models.DateTimeField(auto_now_add=True)
+    expiration_date = models.DateTimeField(blank=True, null=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.processed_at and not self.expiration_date:
+            self.expiration_date = self.processed_at + timedelta(hours=5)
+
+    def __str__(self):
+        return f'{self.pk} content'
