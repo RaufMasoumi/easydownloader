e12f3c1c42535183802ca31112e3dd4d6fa67862
Rauf Masoumi <raouf.rm93@gmail.com>
2025-02-13 14:36:39 +0330

    Added DownloadedContentResponseView

--- downloader/urls.py
+++ downloader/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import DownloadedContentResponseView
 
 urlpatterns = [
-
+    path('<uuid:pk>/', DownloadedContentResponseView.as_view(), name='download')
 ]
--- downloader/views.py
+++ downloader/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import FileResponse, HttpResponseNotFound
+from django.views.generic import View
+from .models import Content
 # Create your views here.
+
+
+class DownloadedContentResponseView(View):
+
+    def get(self, request, pk, **kwargs):
+        content = get_object_or_404(Content, pk=pk)
+        if content.download_url:
+            return redirect(content.download_url)
+        elif content.download_path:
+            try:
+                file = open(content.download_path, 'rb')
+            except FileNotFoundError:
+                return HttpResponseNotFound('There is no correct data for the content to download!')
+            else:
+                return FileResponse(file, as_attachment=True, filename=content.download_path)
+        else:
+            return HttpResponseNotFound('There is no data for the content to download!')
+
+    def post(self, request, pk, **kwargs):
+        return self.get(request, pk, **kwargs)
